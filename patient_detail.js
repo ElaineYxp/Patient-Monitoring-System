c290664ccbcc53d5e6624be65c6b4507dac7655a
ElaineYxp <elaine_yxp@hotmail.com>
2015-11-28 17:22:58 +0800

    First version of patient_detail.js

---
+++ patient_detail.js
@@ -0,0 +1,10 @@
+function showBox(box)
+{
+    var boxToShow = document.getElementById(box);
+    boxToShow.style.display = 'block';
+}
+function clearBox(box)
+{
+    var boxToClear = document.getElementById(box);
+    boxToClear.style.display = 'none';
+}
